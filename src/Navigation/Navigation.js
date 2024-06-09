5a50134ed74b7bfe99b4c3f394310da846ee59eb
gon <jrteab99@gmail.com>
2024-06-09 19:56:44 +0900

    feat: 네비바 HTML 구조 및 스타일 정의
    - 네비게이션 컴포넌트 HTML 구조 와 스타일 정의
    - 공통 스타일(index.css) 정의
    - 리액트 라우터,리액트 아이콘 설치

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import'./Navigation.css'
+import React from "react";
+import "./Navigation.css";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
 
 function Navigation() {
   return (
-    <div>Navigation</div>
-  )
+    <nav>
+      <div className="nav-container">
+        <input type="text" className="search-input" placeholder="상품 검색" />
+      </div>
+
+      <div className="profile-container">
+        {/* 라우터 링크로 교체 */}
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
