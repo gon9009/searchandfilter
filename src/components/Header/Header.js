50f3547ec15f4bd08538bd6f29f12a7f4b3c8c36
gon <jrteab99@gmail.com>
2024-06-29 07:18:03 +0900

    feat:버튼,헤더 UI 개선
    - button.scss 사이드바/컨텐츠 버튼 스타일링
    - 웹사이트 헤더,폰트,배경 이미지

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./Header.scss";
 import EmojiSearch from "../Emoji/EmojiSearch/EmojiSearch";
 
-function Header({setSearch}) {
+function Header({ setSearch }) {
   return (
     <header>
       <div className="header-container">
-        <h2 className="title">EmojiFinder</h2>
+        <h2 className="title">
+          Emoji
+          <p>Hub</p>
+        </h2>
         <EmojiSearch setSearch={setSearch} />
       </div>
     </header>
